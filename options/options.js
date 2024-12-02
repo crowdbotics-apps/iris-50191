5f697b06ec0c5566b2aecca8b488b661f9c7b663
Crowdbotics <team@crowdbotics.com>
2024-12-02 07:51:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "iris-50191",
+  url: "https://iris-50191.botics.co",
+  api: "https://iris-50191.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
